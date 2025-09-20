47b33062655d8c15aa3b70d2e865073a540f7842
Arthur Schevey <arthur.schevey@gmail.com>
2025-09-20 18:03:24 -0400

    Remove tailwind config

---
+++ src/content/writeups.ts
@@ -0,0 +1,12 @@
+export const writeups = [
+  {
+    title: "February 22nd Recycling Haul",
+    link: "https://garden.arthurs.dev/February-22nd-Recycling-Haul",
+    date: "Apr 25, 2025"
+  },
+  {
+    title: "To Recycle or Not Recycle: Pizza Boxes",
+    link: "https://garden.arthurs.dev/To-Recycle-or-Not-Recycle---Pizza-Boxes",
+    date: "Apr 25, 2025"
+  },
+];
